96554428d9674baf1b878bb34296974822c03e53
rgieseke <none@none>
2010-11-30 20:41:31 +0100

    Go to all views.

--- theming.lua
+++ theming.lua
@@ -5,6 +5,8 @@ module('_m.common.theming', package.seeall)
 function set_buffer_properties()
   local buffer = buffer
   local c = _SCINTILLA.constants
+  buffer.view_ws = 1
+  buffer.virtual_space_options = 1 -- enabled only for rectangular selection
   buffer.edge_column = 80
   buffer.edge_mode = 1
   buffer.multiple_selection = true
@@ -23,7 +25,7 @@ events.connect('buffer_new', m_common.set_buffer_properties)
 events.connect('view_new', m_common.set_buffer_properties)
 events.connect('reset_after', function ()
   for i=1, #_VIEWS do
-    gui.goto_view(1, true)
+    gui.goto_view(1, false)
     view:focus()
     set_buffer_properties()
   end
