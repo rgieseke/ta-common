a2dd91589fcd33f277179985156034772071ea50
rgieseke <none@none>
2010-12-01 21:47:47 +0100

    Updated for 3.5 changes.

--- enclose.lua
+++ enclose.lua
@@ -7,21 +7,20 @@ module('_m.common.enclose', package.seeall)
 -- inserting a single enclosure (Ctrl-Key).
 
 local events = _G.events
-local enclosure = _m.textadept.editing.enclosure
 local string_char = _G.string.char
 local m_editing = _m.textadept.editing
 
-function enclose_selection(str)
+function enclose_selection(left, right)
   if buffer.get_sel_text() == '' then
     return false
   else
-    m_editing.enclose(str)
+    m_editing.enclose(left, right)
   end
 end
 
-function paste_or_grow_enclose (str)
+function paste_or_grow_enclose (left, right)
   if buffer:get_sel_text() == '' then
-    buffer:add_text(enclosure[str].left)
+    buffer:add_text(left)
     return
   else
     start = buffer.anchor
@@ -29,22 +28,22 @@ function paste_or_grow_enclose (str)
     if start > stop then
       start, stop = stop, start
     end
-    add_start = #m_editing.enclosure[str].left
-    add_stop = #m_editing.enclosure[str].right
-    m_editing.enclose(str)
-    buffer:set_sel(start, stop+add_start+add_stop)
+    add_start = #left
+    add_stop = #right
+    m_editing.enclose(left, right)
+    buffer:set_sel(start, stop + add_start + add_stop)
   end
 end
 
 local keys = _G.keys
-keys["'"] = { enclose_selection, 'sng_quotes' }
-keys['"'] = { enclose_selection, 'dbl_quotes' }
-keys['('] = { enclose_selection, 'parens' }
-keys['['] = { enclose_selection, 'brackets' }
-keys['{'] = { enclose_selection, 'braces' }
+keys["'"] = { enclose_selection, "'", "'" }
+keys['"'] = { enclose_selection, '"', '"' }
+keys['('] = { enclose_selection, '(', ')' }
+keys['['] = { enclose_selection, '[', ']' }
+keys['{'] = { enclose_selection, '{', '}' }
 
-keys["c'"] = { paste_or_grow_enclose, 'sng_quotes' }
-keys['c"'] = { paste_or_grow_enclose, 'dbl_quotes' }
-keys['c('] = { paste_or_grow_enclose, 'parens' }
-keys['c['] = { paste_or_grow_enclose, 'brackets' }
-keys['c{'] = { paste_or_grow_enclose, 'braces' }
+keys["c'"] = { paste_or_grow_enclose, "'", "'" }
+keys['c"'] = { paste_or_grow_enclose, '"', '"' }
+keys['c('] = { paste_or_grow_enclose, '(', ')' }
+keys['c['] = { paste_or_grow_enclose, '[', ']' }
+keys['c{'] = { paste_or_grow_enclose, '{', '}' }
