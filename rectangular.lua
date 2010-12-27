b4c17b8ea6a7f14e658425e396a069ab46280f73
rgieseke <robert.gieseke@gmail.com>
2010-12-27 15:28:33 +0100

    Formatting.

--- rectangular.lua
+++ rectangular.lua
@@ -2,30 +2,31 @@
 -- press alt r
 -- keep alt down and use arrow keys to select
 
-keys.ar = { function()
-  local events = events
-  local gui = gui
-  local KEYSYMS = _m.textadept.keys.KEYSYMS
-    gui.statusbar_text = 'Rectangular selection'
-    rselect = events.connect('keypress',
-      function(code, shift, control, alt)
-        if alt and KEYSYMS[code] == 'left' then
-          buffer:char_left_rect_extend()
-          return true
-        elseif alt and KEYSYMS[code] == 'up' then
-          buffer:line_up_rect_extend()
-          return true
-        elseif alt and KEYSYMS[code] == 'right' then
-          buffer:char_right_rect_extend()
-          return true
-        elseif alt and KEYSYMS[code] == 'down' then
-          buffer:line_down_rect_extend()
-          return true
-        else
-          events.disconnect('keypress', rselect)
-          events.emit('update_ui')
-          return
-         end
-      end, 1)
-end
+keys.ar = {
+  function()
+    local events = events
+    local gui = gui
+    local KEYSYMS = _m.textadept.keys.KEYSYMS
+      gui.statusbar_text = 'Rectangular selection'
+      rselect = events.connect('keypress',
+        function(code, shift, control, alt)
+          if alt and KEYSYMS[code] == 'left' then
+            buffer:char_left_rect_extend()
+            return true
+          elseif alt and KEYSYMS[code] == 'up' then
+            buffer:line_up_rect_extend()
+            return true
+          elseif alt and KEYSYMS[code] == 'right' then
+            buffer:char_right_rect_extend()
+            return true
+          elseif alt and KEYSYMS[code] == 'down' then
+            buffer:line_down_rect_extend()
+            return true
+          else
+            events.disconnect('keypress', rselect)
+            events.emit('update_ui')
+            return
+           end
+        end, 1)
+  end
 }
