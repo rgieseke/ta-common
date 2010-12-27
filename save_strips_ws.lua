4f052e9987f12bc8a79244da988e887a852d2507
rgieseke <robert.gieseke@gmail.com>
2010-12-27 15:29:48 +0100

    Variable names, formatting.

--- save_strips_ws.lua
+++ save_strips_ws.lua
@@ -1,26 +1,26 @@
 module('_m.common.save_strips_ws', package.seeall)
 
+local saved_col
+
 events.connect('file_before_save',
   function()
     local buffer = buffer
-    _m.common.save_strips_ws.col = buffer.column[buffer.current_pos]
-  end, 1
-)
+    saved_col = buffer.column[buffer.current_pos]
+  end, 1)
 
 events.connect('file_after_save',
   function()
     local buffer = buffer
-    if _m.common.save_strips_ws.col > 0 then
-      _m.common.save_strips_ws.virtual_space = buffer.virtual_space_options
+    if saved_col > 0 then
+      virtual_space = buffer.virtual_space_options
       buffer.virtual_space_options = 2
       local col = buffer.column[buffer.current_pos]
-      local diff = _m.common.save_strips_ws.col - col
+      local diff = saved_col - col
       if diff > 0 then
         for i=1, diff do
           buffer:char_right()
         end
       end
-      buffer.virtual_space_options = _m.common.save_strips_ws.virtual_space
+      buffer.virtual_space_options = virtual_space
     end
-  end
-)
+  end)
