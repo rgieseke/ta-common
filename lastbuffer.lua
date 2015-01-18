9931592b20e6e4f20ab20e4651bb269a9b7b79c1
Robert Gieseke <rob.g@web.de>
2015-01-18 16:26:02 +0100

    Code style updates

--- lastbuffer.lua
+++ lastbuffer.lua
@@ -4,11 +4,10 @@ local M = {}
 -- ## Commands
 
 -- Save the buffer index before switching.
-events.connect('buffer_before_switch',
-  function()
+events.connect(events.BUFFER_BEFORE_SWITCH, function()
     for index, b in ipairs(_BUFFERS) do
       if b == buffer then
-        last_buffer_index = index
+        last_buffer = b
         break
       end
     end
@@ -16,8 +15,8 @@ events.connect('buffer_before_switch',
 
 -- Switch to last buffer.
 function M.last_buffer()
-  if last_buffer_index and #_BUFFERS >= last_buffer_index then
-    view:goto_buffer(last_buffer_index)
+  if last_buffer and _BUFFERS[last_buffer] then
+    view:goto_buffer(_BUFFERS[last_buffer])
   end
 end
 
